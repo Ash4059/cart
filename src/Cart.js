0b171f92ebcb33b6353564524607118f2ae022a9
zeuslearning-ashish-jaiswal <ashish.jaiswal@zeuslearning.com>
2023-02-05 22:41:35 +0530

    add props in cartitem

--- src/Cart.js
+++ src/Cart.js
@@ -2,14 +2,45 @@ import React from "react";
 import CartItem from "./CartItem";
 
 class Cart extends React.Component{
+
+    constructor(){
+        super();
+        this.state = {
+            products:[
+                {
+                    price: 99,
+                    title: "Watch",
+                    qty: 3,
+                    img: "",
+                    id: 1
+                },
+                {
+                    price: 999,
+                    title: "Mobile phone",
+                    qty: 10,
+                    img: "",
+                    id: 2
+                },
+                {
+                    price: 1999,
+                    title: "Laptop",
+                    qty: 2,
+                    img: "",
+                    id: 3
+                }
+            ]
+        }
+    }
+
     render(){
+        const products = this.state.products;
         return(
             <div className="cart">
-                <CartItem />
-                <CartItem />
-                <CartItem />
-                <CartItem />
-                <CartItem />
+                {
+                    products.map((product) =>{
+                        return <CartItem product={product} key={product.id} />
+                    })
+                }
             </div>
         );
     }
--- src/CartItem.js
+++ src/CartItem.js
@@ -2,16 +2,6 @@ import React from "react";
 
 class CartItem extends React.Component{
 
-    constructor(){
-        super();
-        this.state = {
-            price: 999,
-            title: "phone",
-            qty: 1,
-            img: ""
-        }
-    }
-
     increaseQty = ()=>{
         this.setState((previousState)=>{ return { qty : previousState.qty + 1}});
     }
@@ -25,7 +15,7 @@ class CartItem extends React.Component{
     } 
 
     render(){
-        const {price,title,qty,img} = this.state;
+        const {price,title,qty,img} = this.props.product;
         return(
             <div className="cart-item">
                 <div className="left-block">
